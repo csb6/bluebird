dd4d2791de397f57a57fc94aff25113a8ef8628d
csb6 <cblakley15@gmail.com>
2020-12-15 22:32:54 -0700

    Added more complete typchecking for assignments and initializations; factored out type mismatch error printing code into its own function

--- checker.cpp
+++ checker.cpp
@@ -23,12 +23,37 @@ Checker::Checker(const std::vector<Function*>& functions,
     : m_functions(functions), m_types(types)
 {}
 
+template<typename Other>
+static void print_type_mismatch(unsigned int line_num, const Expression* expr,
+                                const Other* other, const Type* other_type,
+                                const char* other_label = "",
+                                const char* expr_label = "Expression",
+                                const TokenType* op = nullptr)
+{
+    std::cerr << "In statement starting at line " << line_num
+              << ":\n Types don't match:\n  ";
+
+    // Left
+    std::cerr << expr_label << ": ";
+    expr->print(std::cerr);
+    std::cerr << '\t';
+    expr->type()->print(std::cerr);
+    // Op
+    if(op != nullptr) {
+        std::cerr << "  Operator: " << *op << '\n';
+    }
+    // Right
+    std::cerr << "  ";
+    if(other_label[0] != '\0') {
+        std::cerr << other_label << ": ";
+    }
+    other->print(std::cerr);
+    std::cerr << '\t';
+    other_type->print(std::cerr);
+}
+
 // No need to typecheck for literal/lvalue expressions since they aren't composite,
 // so each of their check_types() member functions are empty, defined in header
-
-// TODO: check that unary operators are valid for their values
-void UnaryExpression::check_types(const Statement*) const
-{}
 
 template<typename Other>
 static void check_literal_types(const IntLiteral* literal, const Other* other,
@@ -51,16 +76,15 @@ static void check_literal_types(const IntLiteral* literal, const Other* other,
             exit(1);
         }
     } else {
-        std::cerr << "In statement starting at line " << stmt->line_num
-                  << ":\n Types don't match:\n  IntLiteral: ";
-        literal->print(std::cerr);
-        std::cerr << "\n  Used with: ";
-        other->print(std::cerr);
-        std::cerr << '\t';
-        other_type->print(std::cerr);
+        print_type_mismatch(stmt->line_num, literal,
+                            other, other_type, "Used with", "IntLiteral");
         exit(1);
     }
 }
+
+// TODO: check that unary operators are valid for their values
+void UnaryExpression::check_types(const Statement*) const
+{}
 
 // TODO: check if this binary op is legal for this type
 void BinaryExpression::check_types(const Statement* statement) const
@@ -70,15 +94,8 @@ void BinaryExpression::check_types(const Statement* statement) const
     right->check_types(statement);
     // Check that the types of both sides of the operator match
     if(left->type() != right->type()) {
-        std::cerr << "In statement starting at line " << statement->line_num
-                  << ":\n Types don't match:\n  Left: ";
-        left->print(std::cerr);
-        std::cerr << '\t';
-        left->type()->print(std::cerr);
-        std::cerr << "  Operator: " << op << "\n  Right: ";
-        right->print(std::cerr);
-        std::cerr << '\t';
-        right->type()->print(std::cerr);
+        print_type_mismatch(statement->line_num, left.get(), right.get(), right->type(),
+                            "Right", "Left", &op);
         exit(1);
     } else if(right->kind() == ExpressionKind::IntLiteral) {
         check_literal_types(static_cast<const IntLiteral*>(right.get()), left.get(),
@@ -106,16 +123,8 @@ void FunctionCall::check_types(const Statement* statement) const
         // Make sure each arg type matches corresponding parameter type
         const LValue* param = function->parameters[i];
         if(arg->type() != param->type) {
-            std::cerr << "In statement starting at line " << statement->line_num
-                      << ":\n Argument type doesn't match expected type:\n"
-                         "  Argument: ";
-            arg->print(std::cerr);
-            std::cerr << '\t';
-            arg->type()->print(std::cerr);
-            std::cerr << "  Expected: ";
-            param->print(std::cerr);
-            std::cerr << '\t';
-            param->type->print(std::cerr);
+            print_type_mismatch(statement->line_num, arg, param, param->type,
+                                "Expected function parameter", "Actual function argument");
             exit(1);
         } else if(arg->kind() == ExpressionKind::IntLiteral) {
             check_literal_types(static_cast<const IntLiteral*>(arg), param,
@@ -134,11 +143,19 @@ void Initialization::check_types() const
     if(expression == nullptr)
         return;
     expression->check_types(this);
+    if(expression->type() != lvalue->type) {
+        print_type_mismatch(line_num, expression.get(), lvalue, lvalue->type);
+        exit(1);
+    }
 }
 
 void Assignment::check_types() const
 {
     expression->check_types(this);
+    if(expression->type() != lvalue->type) {
+        print_type_mismatch(line_num, expression.get(), lvalue, lvalue->type);
+        exit(1);
+    }
 }
 
 void IfBlock::check_types() const
