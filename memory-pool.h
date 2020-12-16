9929d15a1939c8966b592e9a8e9f61374f417c33
csb6 <cblakley15@gmail.com>
2020-12-15 17:45:31 -0700

    Cleaned up the custom memory pool to properly forward constructors and be simpler/using better naming

--- memory-pool.h
+++ memory-pool.h
@@ -25,18 +25,16 @@
 class MemoryPool {
     std::forward_list<char*> m_pools;
     std::forward_list<char*>::iterator m_curr;
-    const size_t m_pool_capacity;
+    const size_t m_block_size;
     size_t m_curr_used = 0;
  public:
-    explicit MemoryPool(size_t size)
-        : m_pool_capacity(size)
+    explicit MemoryPool(size_t size) : m_block_size(size)
     {
-        m_pools.push_front((char*)malloc(size));
+        m_pools.push_front((char*)malloc(m_block_size));
         m_curr = m_pools.begin();
     }
     MemoryPool(const MemoryPool&) = delete;
     MemoryPool& operator=(const MemoryPool&) = delete;
-    MemoryPool(MemoryPool&&) = default;
 
     ~MemoryPool()
     {
@@ -48,22 +46,21 @@ class MemoryPool {
     template<typename T, typename ...Params>
     T* make(Params... args)
     {
-        constexpr size_t object_size = sizeof(T);
         size_t offset = m_curr_used % alignof(T);
-        if(m_curr_used + offset + object_size > m_pool_capacity) {
+        if(m_curr_used + offset + sizeof(T) > m_block_size) {
             add_new_pool();
             offset = 0;
         }
 
         m_curr_used += offset;
-        T* object = new (*m_curr + m_curr_used) T{args...};
-        m_curr_used += object_size;
+        T* object = new (*m_curr + m_curr_used) T{std::forward<Params>(args)...};
+        m_curr_used += sizeof(T);
         return object;
     }
  private:
     void add_new_pool()
     {
-        m_pools.push_front((char*)malloc(m_pool_capacity));
+        m_pools.push_front((char*)malloc(m_block_size));
         m_curr = m_pools.begin();
         m_curr_used = 0;
     }
